9a93f9823a54fc0befc4af3691b45abdfeaceabc
Lucidastar <lucidahxx@gmail.com>
2018-02-05 17:41:43 +0800

    添加了model

--- app/src/main/java/lucidastar/com/androidart/MainActivity.java
+++ app/src/main/java/lucidastar/com/androidart/MainActivity.java
@@ -1,7 +1,11 @@
 package lucidastar.com.androidart;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import lucidastar.com.androidart.chapter_2.Chapter2Activity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void startNewActivity(Class clazz){
+        startActivity(new Intent(this,clazz));
+    }
+
+    public void btnChapter_1(View view){
+
+    }
+    public void btnChapter_2(View view){
+        startNewActivity(Chapter2Activity.class);
     }
 }
---
+++ app/src/main/java/lucidastar/com/androidart/chapter_2/Chapter2Activity.java
@@ -0,0 +1,15 @@
+package lucidastar.com.androidart.chapter_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lucidastar.com.androidart.R;
+
+public class Chapter2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter2);
+    }
+}
